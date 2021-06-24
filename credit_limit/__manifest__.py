6ca62e32eefb6d0864f162e3b6ddd7514bc00cf8
Kewitz Colina <kewitz.colina@gmail.com>
2021-06-24 15:25:14 -0400

    [WIP] Modulo Limite de Credito en listas de precio.

---
+++ credit_limit/__manifest__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Credit limit",
+    'version': "14.0.1",
+    'author': "Kewitz Colina",
+    'website': "https://github.com/colinak",
+    'summary': "Credit limit in Price List",
+    'sequence': 10,
+    'description': "Credit limit in Price List",
+    'category': "Sales",
+    'depends': [
+        'base',
+        'product',
+        'sale'
+    ],
+    'data': [
+        'views/product_price_list_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
+
---
+++ credit_limit/models/product_price_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+
+class ProductPriceList(models.Model):
+    _inherit = 'product.pricelist.item'
+
+
+    credit_limit = fields.Float(
+        string="Credid Limit",
+        digits=(12,2)
+    )
+
