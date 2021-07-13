5fd159cda7e1dedbb920f7abf63392946c0bcf80
Kewitz Colina <kewitz.colina@gmail.com>
2021-07-13 13:53:03 -0400

    [WIP] Limite de Credito de Cliente en Orden de Venta.

--- credit_limit/models/__init__.py
+++ credit_limit/models/__init__.py
@@ -2,5 +2,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import product_price_list 
+# from . import product_price_list 
+from . import res_partner 
+from . import sale_order 
 
