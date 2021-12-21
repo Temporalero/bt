84e8de3ce2832ac3636b4faeefbafdb30a260bc1
Kewitz Colina <kewitz.colina@gmail.com>
2021-12-21 14:08:03 -0400

    [WIP] Landscape en Format Paper Purchase order

---
+++ format_paper_purchase_report/__manifest__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Format Paper Purchase Report",
+    'version': "14.0.1",
+    'author': "Kewitz Colina",
+    'website': "https://github.com/colinak",
+    'summary': "Hr Employee",
+    'sequence': 100,
+    'category': "Purchase",
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/format_paper_purchase_report.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
+
